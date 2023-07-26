6e7fa0b8ef04f5d72b744e1104715498cbb8d825
vlunic00 <vlunic00@fesb.hr>
2023-07-26 14:09:21 +0200

    More work on cards

--- components/Cards.js
+++ components/Cards.js
@@ -1,41 +1,16 @@
-function Cards(){
+function Cards({ title, description, icon, id, selected, changeSelected }){
     return(
         <>
-<<<<<<< HEAD
-        <div className= "p-10 mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 gap-5 justify-between">
-        <div className="flex mt-8 justify-center">
-            <a href="#" className=" mx-auto block w-80 h-80 bg-gradient-to-b from-gray-600 to-slate-900 rounded-lg shadow">
-                <div className="w-full mt-8 px-3 flex justify-between w-full">
-                    <img src="/images/database.png" className="h-16 ml-2"></img>
-                    <img src="/images/arrow2.svg" className="h-12 mt-2 mr-2"></img>
-                </div>
-                <p className="font-space text-4xl text-white font-bold w-10 mt-8 ml-6">Cloud Storage</p>
-                <p className="font-space text-l text-white mt-2 ml-6">Provides customized solutions to create and maintain software applications.</p>
-            </a>
-=======
-        <div className="flex mt-8">
-            <div className="mx-auto block w-80 h-80 bg-gradient-to-b from-gray-600 to-slate-900 rounded-lg shadowx">
+        <div className="flex mt-8 mr-10 ml-10">
+            <div className={`mx-auto block w-80 h-80 rounded-lg shadowx cursor-pointer ${selected === true ? 'bg-babyblue' : 'bg-gradient-to-b from-gray-600 to-slate-900'}`} onClick={changeSelected}>
                 <div className="w-full mt-8 px-3 flex justify-between">
-                    <img src="/images/database.png" className="h-16 ml-2"></img>
+                    <img src={icon} className="h-16 ml-2"></img>
                     <img src="/images/arrow2.svg" className="h-12 mt-2 mr-2"></img>
                 </div>
-                <p className="font-space font-bold text-4xl text-white w-10 mt-8 ml-6">Cloud storage</p>
-                <p className="font-space text-l text-white mt-2 ml-6">Provides customized solutions to create and maintain software applications.</p>
+                <p className="font-space font-bold text-4xl text-white w-10 mt-8 ml-6">{title}</p>
+                <p className="font-space text-l text-white w-64 mt-2 ml-6">{description}</p>
             </div>
->>>>>>> 9843a0e396dcf1f328245dabcfc2669389990802
         </div>
-        <div className="flex mt-8 justify-center">
-            <a href="#" className=" mx-auto block w-80 h-80 bg-gradient-to-b from-gray-600 to-slate-900 hover:bg-babyblue-500 rounded-lg shadow">
-                <div className="w-full mt-8 px-3 flex justify-between w-full">
-                    <img src="/images/cloud.png" className="h-16 ml-3"></img>
-                    <img src="/images/arrow2.svg" className="h-12 mt-2 mr-2"></img>
-                </div>
-                <p className="font-space text-4xl text-white font-bold w-10 mt-8 ml-6">Cloud Computing</p>
-                <p className="font-space text-l text-white mt-2 ml-6">Provides access to scalable computing resources to optimize their operations.</p>
-            </a>
-        </div>
-        </div>
-
         </>
     )
 }

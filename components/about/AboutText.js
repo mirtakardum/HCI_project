54cee4d1d7e84141bd39c9aaf5eaeb2cf23c83ba
vlunic00 <vlunic00@fesb.hr>
2023-08-21 20:07:39 +0200

    Added back to top in footer

--- components/about/AboutText.js
+++ components/about/AboutText.js
@@ -1,16 +1,16 @@
 function AboutText() {
     return(
         <>
-        <div className="bg-dark h-72">
-        <div className="h-fit">
-            <p className="font-lato font-bold text-xl text-white w-15 mt-40 ml-80 mr-28 text-right">MVK is a full stack cloud hosting</p>
-            <p className="font-lato font-bold text-xl text-white w-15 mt-2 ml-80 mr-28 text-right">company founded in Split.</p>
-            <p className="font-lato font-bold text-xl text-white w-15 mt-2 ml-80 mr-28 text-right">We are currently employing 500+ people</p>
-            <p className="font-lato font-bold text-xl text-white w-15 mt-2 ml-80 mr-28 text-right">in 3 locations; Split, Zagreb, Dugopolje.</p>
+        <div className="bg-dark h-96 lg:h-72">
+        <div className="h-fit lg:w-full w-80 ml-12">
+            <p className="font-lato font-bold text-xl lg:text-2xl text-white w-15 mt-10 lg:mt-40 lg:ml-80 lg:mr-28 text-right">MVK is a full stack cloud hosting</p>
+            <p className="font-lato font-bold text-xl lg:text-2xl text-white w-15 mt-2 lg:ml-80 lg:mr-28 text-right">company founded in Split.</p>
+            <p className="font-lato font-bold text-xl lg:text-2xl text-white w-15 mt-2 lg:ml-80 lg:mr-28 text-right">We are currently employing 500+ people</p>
+            <p className="font-lato font-bold text-xl lg:text-2xl text-white w-15 mt-2 lg:ml-80 lg:mr-28 text-right">in 3 locations; Split, Zagreb, Dugopolje.</p>
         </div>
         <div>
-            <p className="font-space font-bold text-4xl text-white w-15 mt-15 ml-20">We believe that technology</p>
-            <p className="font-space font-bold text-4xl text-white w-15 mt-3 ml-20">can change the world.</p>
+            <p className="font-space font-bold text-2xl lg:text-4xl text-white w-full lg:w-15 mt-16 lg:mt-0 ml-4 lg:ml-20">We believe that technology</p>
+            <p className="font-space font-bold text-2xl lg:text-4xl text-white w-full lg:w-15 mt-3 ml-4 lg:ml-20">can change the world.</p>
         </div>
         </div>
         </>
--- pages/about.js
+++ pages/about.js
@@ -16,11 +16,11 @@ function About(){
       <div className="h-screen bg-dark">
         <Header theme={"dark"} page={"about"}/>
         <PathDisplay title={aboutTitle}/>
-        <div className="absolute top-[17%] right-[25%] h-[4%] w-[2%] z-10 float-right bg-light"></div>
-        <div className="absolute top-[60%] left-[50%] h-[2%] w-[1%] z-10 float-right bg-light"></div>
-        <div className="absolute top-[25%] left-[40%] h-[4%] w-[2%] z-10 float-right bg-light"></div>
-        <div className="absolute top-[40%] right-[10%] h-[6%] w-[3%] z-10 float-right bg-light"></div>
-        <div className="absolute top-[60%] left-[10%] h-[3.5%] w-[2%] z-10 float-right bg-light"></div>
+        <div className="hidden lg:block absolute top-[17%] right-[25%] h-[4%] w-[2%] z-10 float-right bg-light"></div>
+        <div className="hidden lg:block absolute top-[60%] left-[50%] h-[2%] w-[1%] z-10 float-right bg-light"></div>
+        <div className="hidden lg:block absolute top-[25%] left-[40%] h-[4%] w-[2%] z-10 float-right bg-light"></div>
+        <div className="hidden lg:block absolute top-[40%] right-[10%] h-[6%] w-[3%] z-10 float-right bg-light"></div>
+        <div className="hidden lg:block absolute top-[60%] left-[10%] h-[3.5%] w-[2%] z-10 float-right bg-light"></div>
         <AboutText />
         <Founders />
         <Principles />
